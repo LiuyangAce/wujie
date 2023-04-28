8b87161ebc2007f5e6d290dbcab0ad1eed9c273a
liuyang <1773587692@qq.com>
2023-04-28 17:36:20 +0800

    暂存路由

---
+++ v3-child/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+
+const routes = [
+  // {
+  //   path: '/1',
+  //   name: 'v3Child',
+  //   component: ,
+  // },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+})
+
+
+export default router
